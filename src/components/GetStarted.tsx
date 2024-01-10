ec7e60fd664f51f49129984dc902d2c3dc6eb0e6
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-01-10 10:45:38 -0300

    feat: add GetStarted component

---
+++ src/components/GetStarted.tsx
@@ -0,0 +1,16 @@
+const GetStarted = () => {
+  return (
+    <section className="container w-full flex flex-col gap-7 items-center p-5 mt-16">
+        <h1 className="text-4xl text-center capitalize leading-[50px]">Build the community your fans will love</h1>
+        <p className="text-center font-semibold">
+            Huddle re-imagines the way we build communities. You have a voice, bot so does
+            your audience. Create connections with your users as you engage in genuine discussion.
+        </p>   
+        <a href="" className="px-10 py-3 bg-tPink text-white rounded-full shadow-md hover:bg-tLightPink transition-all duration-[.4s] font-semibold">
+            Get Started For Free
+        </a> 
+    </section>
+  )
+}
+
+export default GetStarted
