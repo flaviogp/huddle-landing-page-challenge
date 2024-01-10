a5266bbe957333e9712695595829c7d35e3b452b
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-01-10 10:46:10 -0300

    feat: add background images to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,20 @@ export default {
         'tLightRed': 'hsl(0, 100%, 63%)',
         'tVeryDarkCyan': 'hsl(192, 100%, 9%)',
         'tVeryPaleBlue': 'hsl(207, 100%, 98%)',
-      }
+      },
+      backgroundImage: {
+        'desktopBgFooterTop': 'url(./assets/images/bg-footer-top-desktop.svg)',
+        'mobileBgFooterTop': 'url(./assets/images/bg-footer-top-mobile.svg)',
+        'desktopBgSectionBotton1': 'url(./assets/images/bg-section-bottom-desktop-1.svg)',
+        'desktopBgSectionBotton2': 'url(./assets/images/bg-section-bottom-desktop-2.svg)',
+        'mobileBgSectionBotton1': 'url(./assets/images/bg-section-bottom-mobile-1.svg)',
+        'mobileBgSectionBotton2': 'url(./assets/images/bg-section-bottom-mobile-1.svg)',
+        'desktopBgSectionTop1': 'url(./assets/images/bg-section-top-desktop-1.svg)',
+        'desktopBgSectionTop2': 'url(./assets/images/bg-section-top-desktop-2.svg)',
+        'mobileBgSectionTop1': 'url(./assets/images/bg-section-top-mobile-1.svg)',
+        'mobileBgSectionTop2': 'url(./assets/images/bg-section-top-mobile-1.svg)',
+
+      },
     },
   },
   plugins: [],
