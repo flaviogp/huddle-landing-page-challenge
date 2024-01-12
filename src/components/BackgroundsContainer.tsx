62f23665133ddae2d6f5f2ab5899bbe51a0e349d
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-01-12 09:43:23 -0300

    update: update BackgroundContainer to diferents backgrounds or none

--- src/components/BackgroundsContainer.tsx
+++ src/components/BackgroundsContainer.tsx
@@ -3,16 +3,31 @@ import { ReactElement } from "react"
 
 interface BackgroundContainerProps {
     children: ReactElement;
-    bgTop: string;
-    bgBottom: string;
+    bgTop?: string;
+    bgBottom?: string;
 }
 
 const BackgroundsContainer = ({children, bgBottom, bgTop}: BackgroundContainerProps) => {
   return (
     <div>
-       <div className={`w-full h-[150px] ${bgTop == 'top1' ? 'bg-mobileBgSectionTop1' : bgTop == 'top2' && 'bg-mobileBgSectionTop2'} bg-contain bg-no-repeat`}></div>
-        {children}
-       <div className={`w-full h-[150px] ${bgBottom == 'bottom1' ? 'bg-mobileBgSectionBottom1' : bgBottom == 'bottom2' && 'bg-mobileBgSectionBottom2'} bg-contain bg-no-repeat`}></div>
+        {
+            bgTop == 'top1' &&
+            <div className={`w-full h-[150px] bg-mobileBgSectionTop1 bg-contain bg-no-repeat`}></div>
+        }
+        {
+            bgTop == 'top2' &&
+            <div className={`w-full h-[150px] bg-mobileBgSectionTop2 bg-contain bg-no-repeat`}></div>
+        }
+        {
+            bgTop == 'footer' &&
+            <div className={`w-full h-[150px] bg-mobileBgFooterTop bg-contain bg-no-repeat`}></div>
+        }
+            {children}
+
+        {
+            bgBottom &&
+            <div className={`w-full h-[150px] ${bgBottom == 'bottom1' ? 'bg-mobileBgSectionBottom1' : bgBottom == 'bottom2' && 'bg-mobileBgSectionBottom2'} bg-contain bg-no-repeat`}></div>
+        }
     </div>
   )
 }
