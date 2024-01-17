9e4f48e89d6186dee0dad24f23346aa2b5de7e79
FlavioGP <flavio.gpinheiro@hotmail.com>
2024-01-17 09:28:50 -0300

    feat: form validation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 const Button = () => {
   return (
-    <a href="" className="px-10 py-3 bg-tPink text-white rounded-full shadow-md hover:bg-tLightPink transition-all duration-[.4s] font-semibold">
+    <a href="#" className="px-10 py-3 bg-tPink text-white rounded-full shadow-md hover:bg-tLightPink transition-all duration-[.4s] font-semibold">
         Get Started For Free
     </a> 
 
--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,4 +1,29 @@
+import { useState } from "react"
+
 const Newsletter = () => {
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState('');
+  const [successMessage, setSuccessMessage] = useState('') 
+  
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]/i;
+    const test = emailRegex.test(email)
+
+    if(!test) {
+      setSuccessMessage('')
+      setError( 'Endereço de E-mail invalido!')
+      return;
+    } else {
+      setError('')
+      setSuccessMessage('Obrigado por cadastrar seu email!')
+      return //Function to submit
+    }
+    
+  }
+
+
   return (
     <div className="flex flex-col items-start gap-7 text-white p-7 max-w-[400px] lg:max-w-max">
         <h2 className="uppercase text-2xl">Newsletter</h2>
@@ -7,12 +32,25 @@ const Newsletter = () => {
             newslwtter. We'll never send you spam or pass on your email adress.
         </p>
 
-        <form action="#" className="flex flex-wrap justify-end w-full lg:w-[520px] gap-5 lg:gap-0 lg:justify-between">
-            <input type="email" name="" id="" className="w-full p-2 rounded-md lg:max-w-[330px]"/>
-            <button type="submit" className="py-2 px-9 bg-tPink text-center rounded-md font-semibold">
+        <form name="form" onSubmit={(e) => handleSubmit(e)} method="GET"  className="flex flex-wrap justify-end w-full lg:w-[520px] gap-5 lg:gap-0 lg:justify-between">
+
+            <input 
+              type="email" 
+              name="email" 
+              id="email" 
+              className="w-full p-2 rounded-md lg:max-w-[330px] text-tVeryDarkCyan font-semibold"
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+            <button 
+              type="submit" 
+              className="py-2 px-9 bg-tPink text-center rounded-md font-semibold"
+              
+            >
                 Subscribe
             </button>
         </form>
+        { error && <p className="text-tLightRed">{error}</p> }
+        { successMessage && <p className="text-green-500">{successMessage}</p> }
     </div>
   )
 }
